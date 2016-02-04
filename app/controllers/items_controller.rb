ab12319310669e3e3ec88e9f49babd35b24c111e
Albalenys <albalenysd@gmail.com>
2016-02-04 11:49:45 -0500

    allow users to create new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to "/"
+    else
+      redirect_to new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:text)
+  end
 end
