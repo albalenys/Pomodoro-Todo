910194b71da16d4e16f50ae3196ad53c6808c118
Albalenys <albalenysd@gmail.com>
2016-02-05 13:39:09 -0500

    add simple js timer

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
   $("a.fancybox").fancybox();
+
   $(".fancybox-form").submit(function(event) {
     parent.$.fancybox.close();
   });
+
   $(".fancybox").fancybox({
     type: 'iframe',
     afterClose: function () {
         parent.location.reload(true);
+    }
+  });
+
+  $(".start").submit(function(event) {
+    event.preventDefault();
+    $(".clock-info").empty();
+    var count = 10;
+    var counter = setInterval(timer, 1000);
+
+    function timer() {
+      count -= 1;
+      $(".clock").html(count);
+      if (count <= 0) {
+        clearInterval(counter);
+        $(".clock").html("<p>end!</p>");
+      }
     }
   });
 });
