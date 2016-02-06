5aa0bafd7f4c5f14e18148607cea382e3cd69d0c
Albalenys <albalenysd@gmail.com>
2016-02-05 19:25:09 -0500

    hide clock div in timer page until ready

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
     var seconds = 0;
 
     $(".clock-info").html("<h4>Work in progress...</h4><p>You are working on task <em>" + task + "</em></p>");
+    $( ".clock" ).show();
     $(".minutes").html(minutes);
     $(".seconds").html("0" + seconds);
     var minCounter = setInterval(minTimer, 60000);
