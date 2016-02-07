56d0b1b0c1f6d0c1b4c3e6081e7a56e43ee25c16
Albalenys <albalenysd@gmail.com>
2016-02-07 16:36:16 -0500

    move fancybox init js to seperate file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
-  // Fancybox
-  $("a.fancybox").fancybox();
-
-  $(".fancybox-form").submit(function(event) {
-    parent.$.fancybox.close();
-  });
-
-  $(".fancybox").fancybox({
-    type: 'iframe',
-    afterClose: function () {
-        parent.location.reload(true);
-    }
-  });
+  initFancyBox();
 
   // Ajax
   $("#add-task").on("submit", "form", function(event) {
