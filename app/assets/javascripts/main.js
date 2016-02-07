551d0aec2c982a7912d5c0c64fcb15688683b4f5
Albalenys <albalenysd@gmail.com>
2016-02-07 16:40:42 -0500

    move timer js to seperate file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   initFancyBox();
+  initTimer();
 
-  // Ajax
   $("#add-task").on("submit", "form", function(event) {
     event.preventDefault();
     $.ajax({
@@ -34,43 +34,4 @@ $(document).ready(function() {
     })
   });
 
-  // Timer
-  $(".start").submit(function(event) {
-    event.preventDefault();
-    var task = $("#text").val();
-    var minutes = $("#length").val();
-    var seconds = 0;
-
-    $(".clock-info").html("<h4>Work in progress...</h4><p>You are working on task <em>" + task + "</em></p>");
-    $( ".clock" ).show();
-    $( ".clock-bg" ).show();
-    $(".minutes").html(minutes);
-    $(".seconds").html("0" + seconds);
-    var minCounter = setInterval(minTimer, 60000);
-    var secCounter = setInterval(secTimer, 1000);
-
-    function minTimer() {
-      minutes -= 1;
-      $(".minutes").html(minutes);
-      if (minutes <= 0) {
-        clearInterval(mincounter);
-        alert("You gained a pomodoro!");
-      }
-    }
-
-    function secTimer() {
-      seconds -= 1;
-
-      if (seconds <= 0) {
-        seconds = 59;
-        $(".seconds").html(seconds);
-      }
-      else if (seconds < 10) {
-        $(".seconds").html("0" + seconds);
-      }
-      else {
-        $(".seconds").html(seconds);
-      }
-    }
-  });
 });
