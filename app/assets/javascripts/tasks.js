d8ef956bda23a30f4b3322352e3467be74cc3dcd
Albalenys <albalenysd@gmail.com>
2016-02-09 03:01:42 -0500

    move ajax for tasks into seperate file

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,33 @@
+function taskAjax() {
+  $("#add-task").on("submit", "form", function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      method: $(this).attr('method')
+    }).done(function(response) {
+      newTask = $(response).find(".list li").last();
+      $(".list").append(newTask[0]);
+      $("#add-task form").trigger("reset");
+    })
+  });
+
+  $(document).on("click", ".delete-task", function(event) {
+    event.preventDefault();
+    $(event.target).closest("li").remove();
+  });
+
+  $(".edit-task").click(function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      method: $(this).attr('method')
+    }).done(function(response) {
+      editForm = $(event.target).closest("li").find(".task-form");
+      currentTask = $(event.target).closest("li").find(".task");
+      editForm.show();
+      currentTask.hide();
+    })
+  });
+}
