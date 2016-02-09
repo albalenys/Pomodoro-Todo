a800391cbd928d6a9101278bae372d651151077d
Albalenys <albalenysd@gmail.com>
2016-02-09 02:18:22 -0500

    place constraints on timer

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -1,5 +1,5 @@
 class Pomodoro < ActiveRecord::Base
-  validates :length, presence: true
+  validates :length, :numericality => { greater_than_or_equal_to: 10, less_than_or_equal_to: 59 }, presence: true
 
   belongs_to :task
   belongs_to :user
